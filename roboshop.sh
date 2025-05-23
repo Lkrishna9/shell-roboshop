0b84b6fe6a049312360ed1102943ae5a76848fca
Lkrishna9 <kurapatileelakrishna@gmail.com>
2025-05-23 14:57:28 +1000

    colour

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-009826b9b5d621eb1"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalouge" "user" "cart" "shipping" "payment" "dispatch" "fronend")
-
+ZONE_ID="Z07370541QXT18547F7RY"
+DOMAIN_NAME="devpracticers.site"
 for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-009826b9b5d621eb1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
